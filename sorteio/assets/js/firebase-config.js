32e769568422233d599b273f44e9febbb3baca36
yoyozi <41512132+yoyoziyo@users.noreply.github.com>
2026-08-07 20:05:33 -0300

    Add sorteio/assets/js/firebase-config.js

---
+++ sorteio/assets/js/firebase-config.js
@@ -0,0 +1,15 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAVDnhMwbEpSyRN7PI-wRh__qqOfxA410s",
+  authDomain: "sorteio-2eea0.firebaseapp.com",
+  projectId: "sorteio-2eea0",
+  storageBucket: "sorteio-2eea0.firebasestorage.app",
+  messagingSenderId: "221837666778",
+  appId: "1:221837666778:web:164ce6c63ba388c0c6f8e3"
+};
+
+// Usada somente enquanto config/evento ainda não existir no Firestore.
+export const fallbackEvent = {
+  closesAt: "2026-08-07T22:00:00-03:00",
+  title: "Sorteio especial",
+  active: true
+};
