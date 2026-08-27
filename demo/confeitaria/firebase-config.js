2b254c6f46e9d17468593a4a970bb046877a14a1
yoyozi <41512132+yoyoziyo@users.noreply.github.com>
2026-08-27 15:21:05 -0300

    Adicionar firebase-config.js ao YOITES Catalog

---
+++ demo/confeitaria/firebase-config.js
@@ -0,0 +1,11 @@
+export const firebaseConfig={
+  enabled:true,
+  storeId:'confeitaria',
+  apiKey:'AIzaSyAGArg0w_WJJCkCQX20bpvuno-h6Dww5AM',
+  authDomain:'yoites-catalog-demo.firebaseapp.com',
+  projectId:'yoites-catalog-demo',
+  storageBucket:'yoites-catalog-demo.firebasestorage.app',
+  messagingSenderId:'649291595048',
+  appId:'1:649291595048:web:596320e5dfe29dcd9ea41d'
+};
+
